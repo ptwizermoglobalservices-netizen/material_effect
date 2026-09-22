206576b6129eb823f385f95d359d5ba194327cd9
ptwizermoglobalservices-netizen <ptwizermoglobalservices@gmail.com>
2026-09-22 12:53:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ def run_frame_pushover(
         base_shear = -(ops.nodeReaction(1, 1) + ops.nodeReaction(4, 1)) / 1e3
         
         # CorotTruss axial force (N -> kN)
-        brace_axial = ops.eleResponse(4, "axialForce")[0] / 1e3
+        brace_axial = -(ops.eleResponse(4, "axialForce")[0] / 1e3)
 
         D_hist.append(roof_disp)
         V_hist.append(base_shear)
